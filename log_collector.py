1bb756492f819c2815074bdb28ef2383c3b97e25
Glenys29 <a.peoples4205@student.nu.edu>
2025-06-25 15:34:16 +0300

    Update log_collector.py

--- log_collector.py
+++ log_collector.py
@@ -2,12 +2,12 @@ import time
 
 def tail_log(file_path):
     with open(file_path, 'r') as file:
-        file.seek(0, 0)  # Start at the top
+        file.seek(0, 0)  # Start to read the logs from the begining of the file
         for line in file:
-            yield line
+            yield line # yield each line as it is read
         while True:
-            line = file.readline()
+            line = file.readline() # read the next line
             if line:
-                yield line
+                yield line # yield the next line if it is not empty
             else:
                 time.sleep(0.5)
